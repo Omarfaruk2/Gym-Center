08d91ae51c95bc0af04a1d7cd844d0f72b32766d
Omarfaruk4200 <emon35293@gmail.com>
2022-04-18 18:55:14 +0600

    added show

--- src/Pages/ShopDetails/ShopDetails.js
+++ src/Pages/ShopDetails/ShopDetails.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import CustomHook from '../CustomHook/CustomHook'
+import Show from '../ShowSingleShop/Show'
 import "./ShopDetails.css"
 
 const ShopDetails = () => {
+
+    const services = CustomHook("services.json")
+    console.log(services)
+
     const navigate = useNavigate()
     const { id } = useParams()
 
     return (
-        <div style={{ height: "100vh" }}>
+        <div className='mt-5' style={{ height: "100vh" }}>
             <h2>Hello for the {id}</h2>
             <button onClick={() => navigate("/checkout")} className='btn btn-dark'>Checkout</button>
+            {
+                services.map(service =>
+                    <Show
+                        key={service.id}
+                        service={service}
+                    ></Show>
 
+                )
+            }
         </div>
     )
 }
